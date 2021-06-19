26f773a53a1df0ec8680d505dd68103225e1cf4f
zhouhuan <754865975@qq.com>
2021-06-19 11:10:43 +0800

    Updates

---
+++ precache-manifest.250181a5c90255d298d7cf1465e048a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2483252eb6e295e1694c",
+    "url": "/myswap/static/css/main.1cb04d8c.chunk.css"
+  },
+  {
+    "revision": "2483252eb6e295e1694c",
+    "url": "/myswap/static/js/main.2483252e.chunk.js"
+  },
+  {
+    "revision": "519e6f84b095aff19cbb",
+    "url": "/myswap/static/css/1.c2a1934d.chunk.css"
+  },
+  {
+    "revision": "519e6f84b095aff19cbb",
+    "url": "/myswap/static/js/1.519e6f84.chunk.js"
+  },
+  {
+    "revision": "0fbb4ed9379e5c3f9c0a",
+    "url": "/myswap/static/js/runtime~main.0fbb4ed9.js"
+  },
+  {
+    "revision": "4e988ef48802342a05db9efc8eb69a1b",
+    "url": "/myswap/index.html"
+  }
+];
